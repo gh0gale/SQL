452b7ae27a126f4ff6897c67601064870b7eacd2
gh0gale <info.ghogale@gmail.com>
2025-07-30 12:31:21 +0530

    Time: 787 ms (18.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,9 @@
+SELECT 
+  ROUND(SUM(order_date = customer_pref_delivery_date) * 100.0 / COUNT(delivery_id), 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id,order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+    
+)
