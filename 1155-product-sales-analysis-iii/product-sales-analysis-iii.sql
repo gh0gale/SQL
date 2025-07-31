74dd12c93fdf765c97860d1ae41be53214744a74
gh0gale <info.ghogale@gmail.com>
2025-07-31 11:59:58 +0530

    Time: 860 ms (17.63%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 WITH FirstYearSales AS (
-  SELECT product_id, MIN(year) AS first_year
+  SELECT product_id, MIN(year) AS min_year
   FROM Sales
   GROUP BY product_id
 )
 SELECT s.product_id, s.year AS first_year, s.quantity, s.price
 FROM Sales s
 JOIN FirstYearSales f
-  ON s.product_id = f.product_id AND s.year = f.first_year;
+  ON s.product_id = f.product_id AND s.year = f.min_year;
 
