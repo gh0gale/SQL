6131fbe91f9afe57c6df888584f0e60cbc5d9202
gh0gale <info.ghogale@gmail.com>
2025-08-09 20:20:24 +0530

    Time: 814 ms (59.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+SELECT u.name, IFNULL(SUM(distance),0) AS travelled_distance
+FROM Users u
+LEFT JOIN 
+    Rides r
+ON u.id = r.user_id
+GROUP BY u.id
+ORDER BY SUM(distance) DESC,u.name ASC
+
