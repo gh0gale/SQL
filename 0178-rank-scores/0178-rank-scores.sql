86ea80cf648bc84cef4917635aa99d521ccb3fdd
gh0gale <info.ghogale@gmail.com>
2025-08-17 13:46:50 +0530

    Time: 360 ms (68.59%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,8 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT
-    score,
-    DENSE_RANK() OVER (ORDER BY score DESC) AS rank
-FROM
-    Scores
-ORDER BY
-    score DESC;
+SELECT score,
+    DENSE_RANK() OVER(ORDER BY score DESC) AS rank
+FROM Scores
+ORDER BY score DESC;
