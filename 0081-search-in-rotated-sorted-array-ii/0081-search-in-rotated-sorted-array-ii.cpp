ae07fb3e39655f1b16369f29b70b61ea252cc2b5
gh0gale <info.ghogale@gmail.com>
2025-09-02 12:56:31 +0530

    Time: 0 ms (100%), Space: 17.8 MB (89.35%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int target) 
+    {
         int left = 0, right = nums.size() - 1;
 
-        while (left <= right) {
+        while (left <= right) 
+        {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) return true;
+            if (nums[mid] == target)
+            {
+                return true;
+            } 
 
             
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) 
+            {
                 left++;
                 right--;
             }
            
-            else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
+            else if (nums[left] <= nums[mid]) 
+            {
+                if (nums[left] <= target && target < nums[mid]) 
+                {
                     right = mid - 1;
-                } else {
+                } 
+                else 
+                {
                     left = mid + 1;
                 }
             }
             
-            else {
-                if (nums[mid] < target && target <= nums[right]) {
+            else 
+            {
+                if (nums[mid] < target && target <= nums[right]) 
+                {
                     left = mid + 1;
-                } else {
+                } 
+                else 
+                {
                     right = mid - 1;
                 }
             }
